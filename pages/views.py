a220b82591c6b3f97fac536c6c9ce7c8dea7ad0b
amir <amir.espahbody@gmail.com>
2021-02-22 22:24:32 +0330

    function base view (render) and templates

--- pages/views.py
+++ pages/views.py
@@ -3,7 +3,16 @@ from django.shortcuts import render,HttpResponse
 # Create your views here.
 
 def HomePageView(request):
-    return HttpResponse("helloworld")
+    if (request.method=='GET'):
+        context={
+            "messege":"helloworld"
+        }
+        return render(request,'pages/index.html',context)
+
 
 def AboutPageView(request):
-    return HttpResponse("why do you want to know mee?")
+    if (request.method=='GET'):
+        context={
+            "messege":"why do you want to know mee?"
+        }
+        return render(request,'pages/about.html',context)
